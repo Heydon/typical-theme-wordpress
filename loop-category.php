7b86972e543e1296e1217c50d3027e92b0ff61d4
Heydon Pickering <heydon@heydonworks.com>
2012-11-18 23:28:05 +0000

    Correct itemscope on category loop

--- loop-category.php
+++ loop-category.php
@@ -9,7 +9,7 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article itemscope itemtype="http://schema.org/Article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	 
 		<header>
 			<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'typical' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
